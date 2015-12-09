84eaee59d3eededa357b81ee697c3d882008a42d
Bee <beetran21@yahoo.com>
2015-12-09 05:30:08 -0800

    format code

--- bridge/ViewController.swift
+++ bridge/ViewController.swift
@@ -11,24 +11,7 @@ import CoreLocation
 
 var lat: Double = 0;
 var long: Double =  0;
-/*
-public class GeoManager : NSObject, CLLocationManagerDelegate {
-    
-    //MARK: - Properties
-    public var locationManager:CLLocationManager = CLLocationManager()
-    //private(set)  var location:CLLocation?
-    public var location:CLLocation?
-    var locationAuthorized = false
-    public class var sharedInstance: GeoManager {
-        struct SharedInstance {
-            static let instance = GeoManager()
-        }
-        return SharedInstance.instance
-    }
-}
-*/
 
-//let locationManager = CLLocationManager();
 
 class ViewController: UIViewController, UIWebViewDelegate , CLLocationManagerDelegate {
     
@@ -36,21 +19,7 @@ class ViewController: UIViewController, UIWebViewDelegate , CLLocationManagerDel
     
 let locationManager = CLLocationManager();
     
-    var isLoaded = false;
-    
-    // Execute javascript on the page
-    
-    func call_JS(js: String) -> String{
-        var result = "";
-        if(isLoaded){
-            print("Executing javascript: \(js)")
-            result = webView.stringByEvaluatingJavaScriptFromString(js)!
-        } else {
-            print("Trying to run JS before it is loaded \(js)")
-        }
-        return result;
-    }
-    
+   
     // this is the main function to trap JS call via document.location
     // either by click or js function call
     
@@ -58,10 +27,7 @@ let locationManager = CLLocationManager();
         shouldStartLoadWithRequest request: NSURLRequest,
         navigationType nType: UIWebViewNavigationType) -> Bool {
             
-//        var functionName = "";
-//        var param = "";
-//        let urlString = request.URL?.absoluteString;
-     
+   
         //the scheme is the first part of the document.location before th ':'
         //in JS can be anything predefined ie aaa bbb, in this case we use 'bridge:'
         
@@ -69,10 +35,11 @@ let locationManager = CLLocationManager();
             {
                 myrecord = process_scheme((request.URL?.absoluteString)!);
                 
-                switch myrecord.function {
-                case "hello":helloWorld();
-                case "ios_alert":response_call_1(myrecord.param);
-                case "ios_popup": get_new_page(myrecord.param);
+               switch myrecord.function {
+                case "ios_alert":alert("Bridging" , message: myrecord.param);
+                case "ios_popup": pop_new_page(myrecord.param);
+                case "ios_pop_message": pop_message(myrecord.param);
+                case "exchange" : do_exchange(myrecord.param);
                 case "ios_geo":send_geolocation();
                 default : print("Not defined: \(myrecord.function)")
                 }
@@ -87,27 +54,81 @@ let locationManager = CLLocationManager();
     
     // Callback when the webpage is finished loading
     func webViewDidFinishLoad(web: UIWebView){
-        isLoaded = true;
+       
         // looking for the current URL
         let currentURL = web.request?.URL?.absoluteString;
         print("html content loaded! " + currentURL!);
-        
-        let htmlTitle = call_JS("document.title");
-        print("Page title: " + htmlTitle);
 
     }
     
+    func do_exchange(txt: String) {
+        _ = call_js("js_show_data", param: "back to JS: Hi there I got your package");
+    }
+    
+    // run javascript on the page with result = call_js("js_fn", param:"rarameter");
+    func call_js(function_name: String, param: String) -> String{
+        var result = "";
+        var full_path = function_name + "('" + param + "')";
+        print("Run javascript: \(full_path)");
+        
+        result = webView.stringByEvaluatingJavaScriptFromString(full_path)!;
+        return result;
+    }
+    
+    // popup a screen on top of main screen
+    func pop_new_page(txt: String) {
+        
+        //create webview
+        let myWebView:UIWebView = UIWebView(frame: CGRectMake(0, 0, UIScreen.mainScreen().bounds.width, UIScreen.mainScreen().bounds.height ))
+        
+        // format txt to NSURL
+        let full_path = NSURL (string: txt);
+        // detect the protocol
+        let scheme = full_path!.scheme;
+        //get filename put of it
+        let filename = full_path!.lastPathComponent;
+        // get path out of it
+        let pathPrefix = txt.stringByDeletingLastPathComponent;
+        print(pathPrefix + " " + filename!);
+        let root_dir = "";
+        //it is local file
+        var myurl = NSBundle.mainBundle().URLForResource( filename, withExtension: "", subdirectory: root_dir + pathPrefix);
+        //or a link?
+        if (scheme == "http") || (scheme == "https") {
+            myurl = full_path;
+        }
+        
+        let requestObj = NSURLRequest(URL: myurl!);
+        myWebView.loadRequest(requestObj);
+        
+        // add the webview into the uiviewcontroller
+        let screen_name = "popup";
+        // load uiview from nib
+        let viewController = popup(nibName: screen_name, bundle: nil);
+        viewController.view.addSubview(myWebView);
+        viewController.view.sendSubviewToBack(myWebView);
+        
+        // slide in or popup
+        
+        if navigationController != nil {
+            self.navigationController?.pushViewController(viewController, animated: true)
+            
+        } else {
+            presentViewController(viewController, animated: true, completion: nil);
+        }
+        
+    }
+    
     func loadPage(){
-        let localfilePath = NSBundle.mainBundle().URLForResource("index.html", withExtension: "", subdirectory: "html");
-        //let localfilePath = NSBundle.mainBundle().URLForResource("index.html", withExtension: "", subdirectory: "page");
-        //let request = NSURLRequest(URL: NSURL(string:"http://www.google.com")!);
+        let localfilePath = NSBundle.mainBundle().URLForResource("index1.html", withExtension: "", subdirectory: "www");
+
         let request = NSURLRequest(URL: localfilePath!);
         webView.loadRequest(request);
     }
     
     override func viewDidLoad() {
         super.viewDidLoad();
-        if !is_network_on() {  alert("Konvo", message: "An internet connection is required to use Konvo application"); }
+        if !is_network_on() {  alert("", message: "An internet connection is required to use application"); }
    
      // location setting
         locationManager.delegate = self;
@@ -122,7 +143,7 @@ let locationManager = CLLocationManager();
         // important -> need this line of webview wont trap the js calls
         webView.delegate = self;
         webView.scrollView.bounces = false;
-        NSHTTPCookieStorage.sharedHTTPCookieStorage().cookieAcceptPolicy = NSHTTPCookieAcceptPolicy.Always;
+
         loadPage();
     }
     
@@ -157,9 +178,7 @@ let locationManager = CLLocationManager();
         // load uiview from nib
         let viewController = popup(nibName: screen_name, bundle: nil);
 
- //       let viewController = (UIStoryboard(name: screen_name, bundle: nil)).instantiateInitialViewController();
- 
-        
+  
         viewController.view.addSubview(myWebView);
         viewController.view.sendSubviewToBack(myWebView);
 
@@ -215,22 +234,10 @@ let locationManager = CLLocationManager();
     func send_geolocation() {
         let mystring = String (lat) + " , " + String(long);
         let script = "ios_to_js('\(mystring)')";
-        call_JS(script);
+        call_js(script, param:"");
     }
     
         
-    func helloWorld() {
-        print("Hello called from JS")
-        let mystring: String="ok, here the response from SW";
-     
-        let script = "ios_to_js('\(mystring)')";
-        let returnedString = call_JS(script);
-        
-   // anything sent from JS at all?
-        if returnedString.characters.count > 0 {
-                print("the result is \(returnedString)") }
-        
-        }
     
     
 }
